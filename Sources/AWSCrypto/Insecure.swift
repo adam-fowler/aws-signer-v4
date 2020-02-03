a00370a2cca128169bbfcf1c7fb6f0b07c42e856
Adam Fowler <adamfowler71@gmail.com>
2020-02-03 19:48:16 +0000

    Remove all the old crypto files
    Remove old tests
    Remove swift 5.0 as it won't compile

--- Sources/AWSCrypto/Insecure.swift
+++
@@ -1,5 +0,0 @@
-// Insecure.swift
-// based on the Vapor/open-crypto project which tries to replicate the CryptoKit framework interface
-// written by AdamFowler 2020/01/30
-
-public enum Insecure {}
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,9 +1,7 @@
 import XCTest
 
 @testable import AWSSignerTests
-@testable import AWSCryptoTests
 
 XCTMain([
-    testCase(AWSSignerTests.allTests),
-    testCase(AWSCryptoTests.allTests)
+    testCase(AWSSignerTests.allTests)
 ])

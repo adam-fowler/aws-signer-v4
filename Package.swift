a4463a1addc61a6c5a109300a56e17e202ac9d2b
Adam Fowler <adamfowler71@gmail.com>
2020-02-13 10:33:33 +0000

    Ressurected AWSCrypto so we can support earlier OS versions

--- Package.swift
+++ Package.swift
@@ -5,19 +5,32 @@ import PackageDescription
 
 let package = Package(
     name: "AWSSigner",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)],
     products: [
         .library(name: "AWSSigner", targets: ["AWSSigner"]),
         .library(name: "HTTPClientAWSSigner", targets: ["HTTPClientAWSSigner"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/swift-server/async-http-client", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
-        .target(name: "AWSSigner", dependencies: ["Crypto", "NIO", "NIOHTTP1"]),
+        .target(name: "AWSSigner", dependencies: ["AWSCrypto", "NIO", "NIOHTTP1"]),
+        .target(name: "AWSCrypto", dependencies: []),
         .target(name: "HTTPClientAWSSigner", dependencies: ["AWSSigner", "AsyncHTTPClient"]),
+        
         .testTarget(name: "AWSSignerTests", dependencies: ["AWSSigner", "HTTPClientAWSSigner"])
     ]
 )
+
+// switch for whether to use swift crypto. Swift crypto requires macOS10.15 or iOS13.I'd rather not pass this requirement on
+#if os(Linux)
+let useSwiftCrypto = true
+#else
+let useSwiftCrypto = false
+#endif
+
+// Use Swift cypto on Linux.
+if useSwiftCrypto {
+    package.dependencies.append(.package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0"))
+    package.targets.first{$0.name == "AWSCrypto"}?.dependencies.append("Crypto")
+}
